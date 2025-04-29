cd5296af4f6913ce2eb3d46ad9f7b5b4c8ccb92f
lijianhao <eclijianhao@163.com>
2025-04-29 14:09:28 +0800

    添加GA分析代码

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-679ZZ9YPTF"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-679ZZ9YPTF');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
